88da062ca2e6f6bb5da00805e66433a64818ae2e
rivash38 <96996798+rivash38@users.noreply.github.com>
2022-01-04 17:16:54 -0500

    Example of Return Values in Programming
    An example function that returns a value upon completion of running the block of code.

---
+++ returnvalueexample.js
@@ -0,0 +1,9 @@
+// We create a function that accepts two parameters.
+// Integer values for rows and columsn
+function monitorCount(rows, columns)
+{
+    return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
